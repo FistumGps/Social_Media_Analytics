067807feff13787e11facd29fd8a2ba84c8c380e
FistumGps <59232505+FistumGps@users.noreply.github.com>
2022-10-06 10:47:43 +0300

    Update 1_🐤_Twitter.py

--- pages/1_🐤_Twitter.py
+++ pages/1_🐤_Twitter.py
@@ -1,7 +1,7 @@
 # Import base streamlit dependency
 from cgitb import text
 from enum import unique
-from tkinter.font import names
+#from tkinter.font import names
 from turtle import width
 import streamlit as st
 # Import pandas to load the analytics data
